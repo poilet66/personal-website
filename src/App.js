740589966de0bc7037db31248d7cce7d41040e4b
rohanveit1 <rohan.veit@gmail.com>
2024-04-23 20:50:11 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      Hello World!
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        Test
+      </div>
+      <ul className="navbar-links">
+        <li>
+          Test
+        </li>
+        <li>
+          yo
+        </li>
+        <li>
+          1
+        </li>
+        <li>
+          3
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
